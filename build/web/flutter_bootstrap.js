096505dcb2e73d5fb67f25bb5d3843936efd4c84
tokyorose3311-byte <genspark_flutter_dev@genspark.ai>
2026-07-02 14:09:28 +0000

    Add privacy policy page (hosted at /privacy-policy.html)

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -36,4 +36,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded610","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "3274858165"
+  }
+});
